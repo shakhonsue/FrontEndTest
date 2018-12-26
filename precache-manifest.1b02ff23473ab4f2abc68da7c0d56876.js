cd1f8dcc62f2e71229696f19814dadb864ce333b
shakhonsue <shakhonsue@gmail.com>
2018-12-26 17:11:26 +0630

    Updates

---
+++ precache-manifest.1b02ff23473ab4f2abc68da7c0d56876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a824e05bd5f9acff1782",
+    "url": "/FrontEndTest/static/js/runtime~main.a824e05b.js"
+  },
+  {
+    "revision": "b1fa4c38433e57a6330b",
+    "url": "/FrontEndTest/static/js/main.b1fa4c38.chunk.js"
+  },
+  {
+    "revision": "00c31166f8634460c282",
+    "url": "/FrontEndTest/static/js/1.00c31166.chunk.js"
+  },
+  {
+    "revision": "b1fa4c38433e57a6330b",
+    "url": "/FrontEndTest/static/css/main.e6de39c9.chunk.css"
+  },
+  {
+    "revision": "7cfbbc4898611142f5f5438d118fbf89",
+    "url": "/FrontEndTest/index.html"
+  }
+];
